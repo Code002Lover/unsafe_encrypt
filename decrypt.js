e973ddc8c4fbcdf8b1b68c0a0e60654f3101acfe
Mystikfluu <Mystikfluu@gmail.com>
2022-08-06 20:28:18 +0200

    add silent option to decrypt.js
    purely for testing

--- decrypt.js
+++ decrypt.js
@@ -2,13 +2,32 @@ const unsafeencrypt = require("./index.js")
 
 const out = unsafeencrypt.decrypt()
 
+let argv = process.argv.splice(2)
+
+let silent = false
+
+for(let arg of argv) {
+    switch(arg) {
+        case "-s":
+            silent = true
+            break;
+    }
+}
+
 if(out.status == "success") {
+    if(silent)return
     console.log("output:")
     console.log(out.msg)
 } else {
+
+    process.exitCode = 1
+
+    if(silent)return
     console.log("error decrypting the message")
     console.log("perhaps you have the wrong keys?")
     console.log("decrypted message: ", out.msg)
     console.log("signature: ", out.signature)
     console.log("signature expected: ", out.expected)
+
+    
 }

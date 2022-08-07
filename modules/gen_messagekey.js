6cfb10dab21fb4a20848473fa6e726e83b232eed
Mystikfluu <Mystikfluu@gmail.com>
2022-08-07 19:22:47 +0200

    increase default messagekey length

--- modules/gen_messagekey.js
+++ modules/gen_messagekey.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const fs = require("fs");
 
-const messagekey = crypto.randomBytes(256).toString('hex');
+const messagekey = crypto.randomBytes(2**10).toString('hex');
 
 fs.writeFileSync("messagekey.txt",messagekey)

ba62832bfcceb2ccaa3348546806054ff82f6d8c
Mystikfluu <Mystikfluu@gmail.com>
2022-08-07 15:01:18 +0200

    change default signkey length
    improves performance
    lowers security, however 256 bytes are still strong enough

--- modules/gen_signkey.js
+++ modules/gen_signkey.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const fs = require("fs");
 
-const signkey = crypto.randomBytes(2**16).toString('hex');
+const signkey = crypto.randomBytes(2**8).toString('hex');
 
 fs.writeFileSync("signkey.txt",signkey)
